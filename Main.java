b1f04d1bd63eec5b89809041e87018c3f2042e70
Lee, yeyeong <mery0303@naver.com>
2025-03-26 00:00:21 +0900

    fifth

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ public class Main {
 
   public static void main(String[] args) {
     System.out.println("이예영");
-     world.run();
-     Hello.run();
+    World.run();
+    World.run2();
   }
 
 }
--- world.java
+++ world.java
@@ -4,4 +4,8 @@ public class World {
     System.out.println("202214103");
   }
 
+  public static void run2() {
+    System.out.println("hello world");
+  }
+
 }
